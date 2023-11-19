d237489af511002ef2da74ea5edd513019f7d168
Adam Djo <adambenmessaoud2@gmail.com>
2023-11-19 21:44:46 +0100

    update feature

--- app/(models)/Ticket.tsx
+++ app/(models)/Ticket.tsx
@@ -1,11 +1,7 @@
 import { TicketType } from '@/types/ticket.type';
 import mongoose, { Schema, Model } from 'mongoose';
+import connectDB from '../utils/db';
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.Promise = global.Promise;
 const ticketSchema = new Schema<TicketType>(
   {
     title: String,
